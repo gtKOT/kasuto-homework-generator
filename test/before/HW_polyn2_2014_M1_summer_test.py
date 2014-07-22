0618e1bcd651c67b8c293e93656123461ece9cb9
KOT <askastr@gmail.com>
2014-07-23 02:13:15 +0900

    int_coeffのテストを追加

--- test/before/HW_polyn2_2014_M1_summer_test.py
+++ test/before/HW_polyn2_2014_M1_summer_test.py
@@ -8,12 +8,17 @@ import HW_polyn2_2014_M1_summer as hw
 
 
 class TestSgnPrint(unittest.TestCase):
-
     def test(self):
         self.assertEqual(hw.sgn_print(0, False), '+')
         self.assertEqual(hw.sgn_print(0, True),  '')
         self.assertEqual(hw.sgn_print(1, False), '-')
         self.assertEqual(hw.sgn_print(1, True),  '-')
+
+
+class TestIntCoeff(unittest.TestCase):
+    def test(self):
+        self.assertEqual(hw.int_coeff(1), '')
+        self.assertEqual(hw.int_coeff(2), '2')
 
 
 if __name__ == '__main__':
