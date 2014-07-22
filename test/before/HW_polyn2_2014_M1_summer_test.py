f25cb75a40c2aa4247971da82ae0138be7508194
KOT <askastr@gmail.com>
2014-07-23 02:09:52 +0900

    sgn_printのテストを追加

---
+++ test/before/HW_polyn2_2014_M1_summer_test.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+import sys
+sys.path.append('../../src/before')
+
+import unittest
+import HW_polyn2_2014_M1_summer as hw
+
+
+class TestSgnPrint(unittest.TestCase):
+
+    def test(self):
+        self.assertEqual(hw.sgn_print(0, False), '+')
+        self.assertEqual(hw.sgn_print(0, True),  '')
+        self.assertEqual(hw.sgn_print(1, False), '-')
+        self.assertEqual(hw.sgn_print(1, True),  '-')
+
+
+if __name__ == '__main__':
+    unittest.main()
