ac4a8e158157aed258827beb9363a0a89b95d48e
KOT <askastr@gmail.com>
2014-08-02 19:51:16 +0900

    問題情報の持ち方を、[係数の符号配列, 係数の絶対値配列]から、係数配列に変更
    ・乱数の使用順序が変わったため、seedが同じでも生成される問題が以前と異なる
    ・sign、coefficient、constant関数を追加
    ・sgn_print、coeff関数を除去

--- src/after/homework_generator.py
+++ src/after/homework_generator.py
@@ -60,137 +60,124 @@ def qiians(expressions, height_cm, frac=False):
     return r"\qIIans{" + "\n".join(lines) + "\n}{"+str(height_cm)+"cm}"
 
 
-def sgn_print(boolnum, avoid_plus=False):
-    if boolnum == 1:
-        return "-"
-    elif (boolnum == 0) and (not avoid_plus):
+def sign(num):
+    if num > 0:
         return "+"
+    elif num < 0:
+        return "-"
     else:
         return ""
 
 
-# 約分は行わない
-def coeff(numerator, denominator=1):
-    if denominator > 1:
-        return myfrac(numerator, denominator)
-    elif numerator > 1:
-        return str(numerator)
+# 約分は行わない。
+def coefficient(numerator, denominator=1):
+    sgn = sign(numerator * denominator)
+    abs_numerator   = abs(numerator)
+    abs_denominator = abs(denominator)
+
+    if abs_denominator > 1:
+        return sgn + myfrac(abs_numerator, abs_denominator)
+    elif abs_numerator > 1:
+        return sgn + str(abs_numerator)
     else:
+        return sgn
+
+
+def constant(num):
+    if num == 0:
         return ""
+    else:
+        return sign(num) + str(abs(num))
+
+
+def trim_first_plus(str):
+    if len(str) == 0:
+        return ""
+
+    if str[0] == "+":
+        return str[1:]
+    else:
+        return str
 
 
 # (ax+b)
-def bracket_print(sgn_a, a, symbol, sgn_b, b):
-    output = sgn_print(sgn_a, avoid_plus=True) + coeff(a) + symbol
-    output += sgn_print(sgn_b) + str(b)
-    return "(" + output + ")"
+def bracket_print(a, symbol, b):
+    expression = coefficient(a) + symbol + constant(b)
+    return "(" + trim_first_plus(expression) + ")"
 
 
-def int_polyn(symbol, sgn, intdata):
-    output = sgn_print(sgn[0], avoid_plus=True) + coeff(intdata[0])
-    output += bracket_print(sgn[1], intdata[1], symbol, sgn[2], intdata[2])
-    output += sgn_print(sgn[3]) + coeff(intdata[3])
-    output += bracket_print(sgn[4], intdata[4], symbol, sgn[5], intdata[5])
-    return output
+# a(bx+c)+d(ex+f)
+def int_polyn(nums, symbol):
+    expression  = coefficient(nums[0]) + bracket_print(nums[1], symbol, nums[2])
+    expression += coefficient(nums[3]) + bracket_print(nums[4], symbol, nums[5])
+    return trim_first_plus(expression)
 
 
-def frac_polyn(symbol, sgn, intdata):
-    output = sgn_print(sgn[0], avoid_plus=True) + coeff(1, intdata[0])
-    output += bracket_print(sgn[1], intdata[1], symbol, sgn[2], intdata[2])
-    output += sgn_print(sgn[3]) + coeff(1, intdata[3])
-    output += bracket_print(sgn[4], intdata[4], symbol, sgn[5], intdata[5])
-    return output
+# \myfrac{1}{a}(bx+c)+\myfrac{1}{d}(ex+f)
+def frac_polyn(nums, symbol):
+    expression  = coefficient(1, nums[0]) + bracket_print(nums[1], symbol, nums[2])
+    expression += coefficient(1, nums[3]) + bracket_print(nums[4], symbol, nums[5])
+    return trim_first_plus(expression)
 
 
 # ax+b+cx+d
-def tenkaiA(symbol, sgn, intdata):
-    sgn_a = (sgn[0] + sgn[1]) % 2
-    a = intdata[0] * intdata[1]
-    sgn_b = (sgn[0] + sgn[2]) % 2
-    b = intdata[0] * intdata[2]
-    sgn_c = (sgn[3] + sgn[4]) % 2
-    c = intdata[3] * intdata[4]
-    sgn_d = (sgn[3] + sgn[5]) % 2
-    d = intdata[3] * intdata[5]
+def tenkaiA(nums, symbol):
+    a = nums[0] * nums[1]
+    b = nums[0] * nums[2]
+    c = nums[3] * nums[4]
+    d = nums[3] * nums[5]
 
-    output = sgn_print(sgn_a, avoid_plus=True) + coeff(a) + symbol
-    output += sgn_print(sgn_b) + str(b)
-    output += sgn_print(sgn_c) + coeff(c) + symbol
-    output += sgn_print(sgn_d) + str(d)
-    return output
+    expression  = coefficient(a) + symbol + constant(b)
+    expression += coefficient(c) + symbol + constant(d)
+    return trim_first_plus(expression)
 
 
 # ax+b
-def tenkaiB(symbol, sgn, intdata):
-    output = ""
-    a = intdata[0] * intdata[1] * (-1) ** (sgn[0] + sgn[1]) + intdata[3] * intdata[4] * (-1) ** (sgn[3] + sgn[4])
-    b = intdata[0] * intdata[2] * (-1) ** (sgn[0] + sgn[2]) + intdata[3] * intdata[5] * (-1) ** (sgn[3] + sgn[5])
-    avoid_plus = False
+def tenkaiB(nums, symbol):
+    a = nums[0] * nums[1] + nums[3] * nums[4]
+    b = nums[0] * nums[2] + nums[3] * nums[5]
 
-    if a > 0:
-        output += coeff(a) + symbol
-    elif a < 0:
-        output += "-" + coeff(a * (-1)) + symbol
-    else:
-        avoid_plus = True
+    expression = ""
+    if a != 0:
+        expression += coefficient(a) + symbol
+    if b != 0:
+        expression += constant(b)
 
-    if b > 0:
-        output += sgn_print(0, avoid_plus) + str(b)
-    elif b < 0:
-        output += "-" + str(b * (-1))
-    else:
-        if a == 0:
-            output += "0"
-
-    return output
-
-
-# 答えが約分可能かどうか。係数と分母の最大公約数
-def common_factor(sgn, intdata, denominator):
-    coeff = intdata[0] * intdata[1] * (-1) ** (sgn[0] + sgn[1]) + intdata[3] * intdata[4] * (-1) ** (sgn[3] + sgn[4])
-    constt = intdata[0] * intdata[2] * (-1) ** (sgn[0] + sgn[2]) + intdata[3] * intdata[5] * (-1) ** (sgn[3] + sgn[5])
-    return gcd(gcd(abs(coeff), abs(constt)), denominator)
+    return trim_first_plus(expression or "0")
 
 
 # ax+b
 # 約分実行
-def tenkaiB_reduct(symbol, sgn, intdata, g):
-    output = ""
-    a = intdata[0] * intdata[1] * (-1) ** (sgn[0] + sgn[1]) + intdata[3] * intdata[4] * (-1) ** (sgn[3] + sgn[4])
-    b = intdata[0] * intdata[2] * (-1) ** (sgn[0] + sgn[2]) + intdata[3] * intdata[5] * (-1) ** (sgn[3] + sgn[5])
-    avoid_plus = False
-    a = a / g
-    b = b / g
+def tenkaiB_reduct(nums, symbol, g):
+    a = (nums[0] * nums[1] + nums[3] * nums[4]) / g
+    b = (nums[0] * nums[2] + nums[3] * nums[5]) / g
 
-    if a > 0:
-        output += coeff(a) + symbol
-    elif a < 0:
-        output += "-" + coeff(a * (-1)) + symbol
-    else:
-        avoid_plus = True
+    expression = ""
+    if a != 0:
+        expression += coefficient(a) + symbol
+    if b != 0:
+        expression += constant(b)
 
-    if b > 0:
-        output += sgn_print(0, avoid_plus) + str(b)
-    elif b < 0:
-        output += "-" + str(b * (-1))
-    else:
-        if a == 0:
-            output += "0"
+    return trim_first_plus(expression or "0")
 
-    return output
+
+# 答えが約分可能かどうか。係数と分母の最大公約数
+def common_factor(nums, denominator):
+    coeff = nums[0] * nums[1] + nums[3] * nums[4]
+    const = nums[0] * nums[2] + nums[3] * nums[5]
+    return gcd(gcd(coeff, const), denominator)
 
 
 # 排除すべきデータならFalse. 同じ問題と、左右入れ替えただけのものと、カッコの前が+1なものだけ排除
-def isAdmissible(data, data_list):
-    lL = len(data[0])
-    lR = len(data[1])
-    exchange_data = [data[0][lL / 2:lL] + data[0][0:lL / 2], data[1][lR / 2:lR] + data[1][0:lR / 2]]
+def isAdmissible(nums, nums_list):
+    length = len(nums)
+    exchange_nums = nums[length/2:length] + nums[0:length/2]
 
-    a = data not in data_list
-    b = exchange_data not in data_list
-    c = not (data[0][0] == 0 and data[1][0] == 1)
-    d = not (data[0][lL / 2] == 0 and data[1][lR / 2] == 1)
-    e = not (data[1][0] == data[1][3])  # 分数の場合のみ排除したいが今回は排除。分母が同じものは排除。
+    a = nums not in nums_list
+    b = exchange_nums not in nums_list
+    c = not (nums[0] == 1)
+    d = not (nums[3] == 1)
+    e = not (abs(nums[0]) == abs(nums[3]))  # 分数の場合のみ排除したいが今回は排除。分母が同じものは排除。
 
     return a and b and c and d and e
 
@@ -206,15 +193,15 @@ def rand_coeff(probs):
         interval += probs[i]
 
 
-def create_problems_tex(data_list, symbols):
+def create_problems_tex(nums_list, symbols):
     tex_lines = [r"\begin{multienumerate}\restmultienumparameters"]
     at_int_mode = True
 
-    for i in xrange(0, len(data_list), 2):
+    for i in xrange(0, len(nums_list), 2):
         polyn = int_polyn if at_int_mode else frac_polyn
 
-        left_problem  = mathmode(polyn(symbols[i],   sgn=data_list[i][0],   intdata=data_list[i][1]))
-        right_problem = mathmode(polyn(symbols[i+1], sgn=data_list[i+1][0], intdata=data_list[i+1][1]))
+        left_problem  = mathmode(polyn(nums_list[i], symbols[i]))
+        right_problem = mathmode(polyn(nums_list[i+1], symbols[i+1]))
         tex_lines.append( mitemxx(left_problem, right_problem) )
 
         at_int_mode = not at_int_mode  # 2題ごとに整数モードと分数モードの切り替え
@@ -223,7 +210,8 @@ def create_problems_tex(data_list, symbols):
     return "\n".join(tex_lines)
 
 
-def create_answers_tex(data_list, symbols):
+# TODO: max_col
+def create_answers_tex(nums_list, symbols):
     at_frac_mode = True
     at_first_column = True
     col_height = 0.0
@@ -235,41 +223,41 @@ def create_answers_tex(data_list, symbols):
     second_column_tex_lines = []
     current_column_tex_lines = first_column_tex_lines
 
-    for i in xrange(len(data_list)):
+    for i in xrange(len(nums_list)):
         symbol = symbols[i]
-        sgn = data_list[i][0]
-        intdata = data_list[i][1]
+        nums = nums_list[i]
 
         if i % 2 == 0:
             at_frac_mode = not at_frac_mode  # 2題ごとに分数モードと整数モードの切り替え
 
         if at_frac_mode:
-            expressions = [frac_polyn(symbol, sgn, intdata)]
+            expressions = [frac_polyn(nums, symbol)]
 
-            g_int = gcd(intdata[0], intdata[3])
-            denominator = intdata[0] * intdata[3] / g_int
-            intdata[0], intdata[3] = intdata[3] / g_int, intdata[0] / g_int
+            g_int = gcd(nums[0], nums[3])
+            denominator = abs(nums[0] * nums[3] / g_int)
+            nums[0] = denominator / nums[0]
+            nums[3] = denominator / nums[3]
 
             expressions += [
-                myfrac(int_polyn(symbol, sgn, intdata), denominator),
-                myfrac(tenkaiA(symbol, sgn, intdata), denominator),
-                myfrac(tenkaiB(symbol, sgn, intdata), denominator)
+                myfrac(int_polyn(nums, symbol), denominator),
+                myfrac(tenkaiA(nums, symbol), denominator),
+                myfrac(tenkaiB(nums, symbol), denominator)
             ]
 
-            g = common_factor(sgn, intdata, denominator)
-            if g > 1:
+            g = common_factor(nums, denominator)
+            if g > 1:  # 約分
                 if denominator == g:
-                    expressions.append( tenkaiB_reduct(symbol, sgn, intdata, g) )
+                    expressions.append( tenkaiB_reduct(nums, symbol, g) )
                 else:
-                    expressions.append( myfrac(tenkaiB_reduct(symbol, sgn, intdata, g), denominator / g) )
+                    expressions.append( myfrac(tenkaiB_reduct(nums, symbol, g), denominator / g) )
                 problem_height = 4
             else:
                 problem_height = 3.2
         else:
             expressions = [
-                int_polyn(symbol, sgn, intdata),
-                tenkaiA(symbol, sgn, intdata),
-                tenkaiB(symbol, sgn, intdata),
+                int_polyn(nums, symbol),
+                tenkaiA(nums, symbol),
+                tenkaiB(nums, symbol),
             ]
             problem_height = 1.2
 
@@ -320,17 +308,16 @@ if __name__ == "__main__":
 
     # データの生成 -------------------------------------------------------------------
     num_of_problems = 300  # とりあえず300題（2の倍数）
-    data_list = []
+    nums_list = []
     seed = 20140627
     random.seed(seed)
 
-    while len(data_list) < num_of_problems:
-        data = [
-            [random.randint(0, 1) for i in range(6)],    # 符号情報
-            [rand_coeff(coeff_probs) for i in range(6)]  # 絶対値情報
+    while len(nums_list) < num_of_problems:
+        nums = [
+            random.choice([-1, 1]) * rand_coeff(coeff_probs) for i in range(6)
         ]
-        if isAdmissible(data, data_list):
-            data_list.append(data)
+        if isAdmissible(nums, nums_list):
+            nums_list.append(nums)
 
     # 問題ごとの使用文字番号を格納。解答の文字を揃えるため。
     symbols = [symbol_list[rand_coeff(symbol_probs)] for i in xrange(num_of_problems)]
@@ -339,11 +326,11 @@ if __name__ == "__main__":
     header = "% seed: " + str(seed) + "\n"
 
     # 問題の生成 ---------------------------------------------------------------------
-    problems_tex = header + create_problems_tex(data_list, symbols)
+    problems_tex = header + create_problems_tex(nums_list, symbols)
     create_tex_file(datetime.now().strftime("M1a_HW_polyn_%Y%m%d_%H%M_%S"), problems_tex)
     print "problems are generated..."
 
     # 回答の生成 ---------------------------------------------------------------------
-    answers_tex = header + create_answers_tex(data_list, symbols)
+    answers_tex = header + create_answers_tex(nums_list, symbols)
     create_tex_file(datetime.now().strftime("M1a_HW_polynAns_%Y%m%d_%H%M_%S"), answers_tex)
     print "End."
--- test/after/homework_generator_test.py
+++ test/after/homework_generator_test.py
@@ -69,50 +69,75 @@ class TestQiians(unittest.TestCase):
         self.assertEqual(expected, hw.qiians(['2(3x+4)+5(6x+7)', '6x+8+30x+35', '36x+43'], 1.2, True))
 
 
-class TestSgnPrint(unittest.TestCase):
+class TestSign(unittest.TestCase):
     def test(self):
-        self.assertEqual('+', hw.sgn_print(0))
-        self.assertEqual('+', hw.sgn_print(0, False))
-        self.assertEqual('',  hw.sgn_print(0, True))
-        self.assertEqual('-', hw.sgn_print(1))
-        self.assertEqual('-', hw.sgn_print(1, False))
-        self.assertEqual('-', hw.sgn_print(1, True))
+        self.assertEqual('+', hw.sign(1))
+        self.assertEqual('',  hw.sign(0))
+        self.assertEqual('-', hw.sign(-1))
 
 
-class TestCoeff(unittest.TestCase):
+class TestCoefficient(unittest.TestCase):
     def test(self):
-        self.assertEqual('',  hw.coeff(1))
-        self.assertEqual('',  hw.coeff(1, 1))
-        self.assertEqual('2', hw.coeff(2))
-        self.assertEqual('2', hw.coeff(2, 1))
-        self.assertEqual(r'\myfrac{1}{2}', hw.coeff(1, 2))
+        self.assertEqual('+',  hw.coefficient(1))
+        self.assertEqual('+',  hw.coefficient( 1,  1))
+        self.assertEqual('+',  hw.coefficient(-1, -1))
+        self.assertEqual('-',  hw.coefficient(-1))
+        self.assertEqual('-',  hw.coefficient(-1,  1))
+        self.assertEqual('-',  hw.coefficient( 1, -1))
+        self.assertEqual('+2', hw.coefficient(2))
+        self.assertEqual('+2', hw.coefficient( 2,  1))
+        self.assertEqual('+2', hw.coefficient(-2, -1))
+        self.assertEqual('-2', hw.coefficient(-2))
+        self.assertEqual('-2', hw.coefficient( 2, -1))
+        self.assertEqual('-2', hw.coefficient(-2,  1))
+        self.assertEqual(r'+\myfrac{1}{2}', hw.coefficient( 1,  2))
+        self.assertEqual(r'+\myfrac{1}{2}', hw.coefficient(-1, -2))
+        self.assertEqual(r'-\myfrac{1}{2}', hw.coefficient(-1,  2))
+        self.assertEqual(r'-\myfrac{1}{2}', hw.coefficient( 1, -2))
+
+
+class TestConstant(unittest.TestCase):
+    def test(self):
+        self.assertEqual('', hw.constant(0))
+        self.assertEqual('+1', hw.constant(1))
+        self.assertEqual('+2', hw.constant(2))
+        self.assertEqual('-1', hw.constant(-1))
+        self.assertEqual('-2', hw.constant(-2))
+
+
+class TestTrimFirstPlus(unittest.TestCase):
+    def test(self):
+        self.assertEqual('', hw.trim_first_plus(''))
+        self.assertEqual('ax+b', hw.trim_first_plus('ax+b'))
+        self.assertEqual('ax+b', hw.trim_first_plus('+ax+b'))
+        self.assertEqual('-ax+b', hw.trim_first_plus('-ax+b'))
 
 
 class TestBracketPrint(unittest.TestCase):
     def test(self):
-        self.assertEqual('(x+1)',   hw.bracket_print(0, 1, 'x', 0, 1))
-        self.assertEqual('(-y-1)',  hw.bracket_print(1, 1, 'y', 1, 1))
-        self.assertEqual('(2z+2)',  hw.bracket_print(0, 2, 'z', 0, 2))
-        self.assertEqual('(-2w-2)', hw.bracket_print(1, 2, 'w', 1, 2))
+        self.assertEqual('(x+1)',   hw.bracket_print( 1, 'x',  1))
+        self.assertEqual('(-y-1)',  hw.bracket_print(-1, 'y', -1))
+        self.assertEqual('(2z+2)',  hw.bracket_print( 2, 'z',  2))
+        self.assertEqual('(-2w-2)', hw.bracket_print(-2, 'w', -2))
 
 
 class TestIntPolyn(unittest.TestCase):
     def test(self):
         self.assertEqual(
             '(x+2)+(x+3)',
-            hw.int_polyn('x', [0, 0, 0, 0, 0, 0], [1, 1, 2, 1, 1, 3])
+            hw.int_polyn([1, 1, 2, 1, 1, 3], 'x')
         )
         self.assertEqual(
             '2(3y+4)+5(6y+7)',
-            hw.int_polyn('y', [0, 0, 0, 0, 0, 0], [2, 3, 4, 5, 6, 7])
+            hw.int_polyn([2, 3, 4, 5, 6, 7], 'y')
         )
         self.assertEqual(
             '-(-z-2)-(-z-3)',
-            hw.int_polyn('z', [1, 1, 1, 1, 1, 1], [1, 1, 2, 1, 1, 3])
+            hw.int_polyn([-1, -1, -2, -1, -1, -3], 'z')
         )
         self.assertEqual(
             '-2(-3w-4)-5(-6w-7)',
-            hw.int_polyn('w', [1, 1, 1, 1, 1, 1], [2, 3, 4, 5, 6, 7])
+            hw.int_polyn([-2, -3, -4, -5, -6, -7], 'w')
         )
 
 
@@ -120,19 +145,19 @@ class TestFracPolyn(unittest.TestCase):
     def test(self):
         self.assertEqual(
             '(x+2)+(x+3)',
-            hw.frac_polyn('x', [0, 0, 0, 0, 0, 0], [1, 1, 2, 1, 1, 3])
+            hw.frac_polyn([1, 1, 2, 1, 1, 3], 'x')
         )
         self.assertEqual(
             '\myfrac{1}{2}(3y+4)+\myfrac{1}{5}(6y+7)',
-            hw.frac_polyn('y', [0, 0, 0, 0, 0, 0], [2, 3, 4, 5, 6, 7])
+            hw.frac_polyn([2, 3, 4, 5, 6, 7], 'y')
         )
         self.assertEqual(
             '-(-z-2)-(-z-3)',
-            hw.frac_polyn('z', [1, 1, 1, 1, 1, 1], [1, 1, 2, 1, 1, 3])
+            hw.frac_polyn([-1, -1, -2, -1, -1, -3], 'z')
         )
         self.assertEqual(
             '-\myfrac{1}{2}(-3w-4)-\myfrac{1}{5}(-6w-7)',
-            hw.frac_polyn('w', [1, 1, 1, 1, 1, 1], [2, 3, 4, 5, 6, 7])
+            hw.frac_polyn([-2, -3, -4, -5, -6, -7], 'w')
         )
 
 
@@ -140,19 +165,19 @@ class TestTenkaiA(unittest.TestCase):
     def test(self):
         self.assertEqual(
             'x+2+x+3',
-            hw.tenkaiA('x', [0, 0, 0, 0, 0, 0], [1, 1, 2, 1, 1, 3])
+            hw.tenkaiA([1, 1, 2, 1, 1, 3], 'x')
         )
         self.assertEqual(
             '6y+8+30y+35',
-            hw.tenkaiA('y', [0, 0, 0, 0, 0, 0], [2, 3, 4, 5, 6, 7])
+            hw.tenkaiA([2, 3, 4, 5, 6, 7], 'y')
         )
         self.assertEqual(
             '-z-2-z-3',
-            hw.tenkaiA('z', [1, 0, 0, 1, 0, 0], [1, 1, 2, 1, 1, 3])
+            hw.tenkaiA([-1, 1, 2, -1, 1, 3], 'z')
         )
         self.assertEqual(
             '-6w-8-30w-35',
-            hw.tenkaiA('w', [1, 0, 0, 1, 0, 0], [2, 3, 4, 5, 6, 7])
+            hw.tenkaiA([-2, 3, 4, -5, 6, 7], 'w')
         )
 
 
@@ -160,88 +185,89 @@ class TestTenkaiB(unittest.TestCase):
     def test(self):
         self.assertEqual(
             '2x+5',  # (x+2)+(x+3)
-            hw.tenkaiB('x', [0, 0, 0, 0, 0, 0], [1, 1, 2, 1, 1, 3])
+            hw.tenkaiB([1, 1, 2, 1, 1, 3], 'x')
         )
         self.assertEqual(
             '36y+43',  # 2(3y+4)+5(6y+7)
-            hw.tenkaiB('y', [0, 0, 0, 0, 0, 0], [2, 3, 4, 5, 6, 7])
+            hw.tenkaiB([2, 3, 4, 5, 6, 7], 'y')
         )
         self.assertEqual(
             '-2z+5',  # -(z-2)-(z-3)
-            hw.tenkaiB('z', [1, 0, 1, 1, 0, 1], [1, 1, 2, 1, 1, 3])
+            hw.tenkaiB([-1, 1, -2, -1, 1, -3], 'z')
         )
         self.assertEqual(
             '-36w+43',  # -2(3w-4)-5(6w-7)
-            hw.tenkaiB('w', [1, 0, 1, 1, 0, 1], [2, 3, 4, 5, 6, 7])
+            hw.tenkaiB([-2, 3, -4, -5, 6, -7], 'w')
         )
         self.assertEqual(
             '-2a',  # -(a+2)-(a-2)
-            hw.tenkaiB('a', [1, 0, 0, 1, 0, 1], [1, 1, 2, 1, 1, 2])
+            hw.tenkaiB([-1, 1, 2, -1, 1, -2], 'a')
         )
         self.assertEqual(
             '-4',  # -(b+2)-(-b+2)
-            hw.tenkaiB('b', [1, 0, 0, 1, 1, 0], [1, 1, 2, 1, 1, 2])
+            hw.tenkaiB([-1, 1, 2, -1, -1, 2], 'b')
         )
         self.assertEqual(
             '0',  # -(c+2)-(-c-2)
-            hw.tenkaiB('c', [1, 0, 0, 1, 1, 1], [1, 1, 2, 1, 1, 2])
+            hw.tenkaiB([-1, 1, 2, -1, -1, -2], 'c')
         )
 
 
 class TestCommonFactor(unittest.TestCase):
     def test(self):
         # ((x+2)+(x+3)) / 2
-        self.assertEqual(1, hw.common_factor([0, 0, 0, 0, 0, 0], [1, 1, 2, 1, 1, 3], 2))
+        self.assertEqual(1, hw.common_factor([1, 1, 2, 1, 1, 3], 2))
         # ((x+2)+(11x+28)) / 60
-        self.assertEqual(6, hw.common_factor([0, 0, 0, 0, 0, 0], [1, 1, 2, 1, 11, 28], 60))
+        self.assertEqual(6, hw.common_factor([1, 1, 2, 1, 11, 28], 60))
         # ((x+2)+(x-2)) / 2
-        self.assertEqual(2, hw.common_factor([0, 0, 0, 0, 0, 1], [1, 1, 2, 1, 1, 2], 2))
+        self.assertEqual(2, hw.common_factor([1, 1, 2, 1, 1, -2], 2))
         # ((x+2)-(x-2)) / 4
-        self.assertEqual(4, hw.common_factor([0, 0, 0, 0, 1, 0], [1, 1, 2, 1, 1, 2], 4))
+        self.assertEqual(4, hw.common_factor([1, 1, 2, 1, -1, 2], 4))
 
 
 class TestTenkaiBreduct(unittest.TestCase):
     def test(self):
         self.assertEqual(
             '2x+5',  # ((x+2)+(x+3)) / 1
-            hw.tenkaiB_reduct('x', [0, 0, 0, 0, 0, 0], [1, 1, 2, 1, 1, 3], 1)
+            hw.tenkaiB_reduct([1, 1, 2, 1, 1, 3], 'x', 1)
         )
         self.assertEqual(
             '2x+5',  # ((x+2)+(11x+28)) / 6
-            hw.tenkaiB_reduct('x', [0, 0, 0, 0, 0, 0], [1, 1, 2, 1, 11, 28], 6)
+            hw.tenkaiB_reduct([1, 1, 2, 1, 11, 28], 'x', 6)
         )
         self.assertEqual(
             'x',  # ((x+2)+(x-2)) / 2
-            hw.tenkaiB_reduct('x', [0, 0, 0, 0, 0, 1], [1, 1, 2, 1, 1, 2], 2)
+            hw.tenkaiB_reduct([1, 1, 2, 1, 1, -2], 'x', 2)
         )
         self.assertEqual(
             '1',  # ((x+2)-(x-2)) / 4
-            hw.tenkaiB_reduct('x', [0, 0, 0, 0, 1, 0], [1, 1, 2, 1, 1, 2], 4)
+            hw.tenkaiB_reduct([1, 1, 2, 1, -1, 2], 'x', 4)
         )
         self.assertEqual(
             '0',  # ((x+2)-(x+2)) / 2
-            hw.tenkaiB_reduct('x', [0, 0, 0, 1, 0, 0], [1, 1, 2, 1, 1, 2], 2)
+            hw.tenkaiB_reduct([1, 1, 2, -1, 1, 2], 'x', 2)
         )
 
 
 class TestIsAdmissible(unittest.TestCase):
     def test(self):
-        all_plus = [0, 0, 0, 0, 0, 0]
-        self.assertTrue(hw.isAdmissible([all_plus,  [2, 3, 4, 5, 6, 7]], []))
-        self.assertFalse(hw.isAdmissible([all_plus, [2, 3, 4, 5, 6, 7]], [[all_plus, [2, 3, 4, 5, 6, 7]]]))
-        self.assertFalse(hw.isAdmissible([all_plus, [2, 3, 4, 5, 6, 7]], [[all_plus, [5, 6, 7, 2, 3, 4]]]))
-        self.assertFalse(hw.isAdmissible([all_plus, [1, 3, 4, 5, 6, 7]], []))
-        self.assertFalse(hw.isAdmissible([all_plus, [2, 3, 4, 1, 6, 7]], []))
-        self.assertFalse(hw.isAdmissible([all_plus, [2, 3, 4, 2, 6, 7]], []))
+        self.assertTrue(hw.isAdmissible([2, 3, 4, 5, 6, 7], []))
+        self.assertFalse(hw.isAdmissible([2, 3, 4, 5, 6, 7], [[2, 3, 4, 5, 6, 7]]))
+        self.assertFalse(hw.isAdmissible([2, 3, 4, 5, 6, 7], [[5, 6, 7, 2, 3, 4]]))
+        self.assertFalse(hw.isAdmissible([1, 3, 4, 5, 6, 7], []))
+        self.assertFalse(hw.isAdmissible([2, 3, 4, 1, 6, 7], []))
+        self.assertFalse(hw.isAdmissible([2, 3, 4, 2, 6, 7], []))
+        self.assertFalse(hw.isAdmissible([-2, 3, 4,  2, 6, 7], []))
+        self.assertFalse(hw.isAdmissible([ 2, 3, 4, -2, 6, 7], []))
 
 
 class TestCreateProblemsTex(unittest.TestCase):
     def test(self):
         data_list = [
-            [[0, 0, 0, 0, 0, 0], [2, 3, 4, 5, 6, 7]],
-            [[1, 1, 1, 1, 1, 1], [1, 2, 3, 1, 4, 5]],
-            [[0, 0, 0, 0, 0, 0], [2, 3, 4, 5, 6, 7]],
-            [[1, 1, 1, 1, 1, 1], [1, 2, 3, 1, 4, 5]]
+            [2, 3, 4, 5, 6, 7],
+            [-1, -2, -3, -1, -4, -5],
+            [2, 3, 4, 5, 6, 7],
+            [-1, -2, -3, -1, -4, -5]
         ]
         symbols = ['x', 'y', 'x', 'y']
         expected = '\n'.join([
@@ -256,10 +282,10 @@ class TestCreateProblemsTex(unittest.TestCase):
 class TestCreateAnswersTex(unittest.TestCase):
     def test(self):
         data_list = [
-            [[0, 0, 0, 0, 0, 0], [2, 3, 4, 5, 6, 7]],
-            [[1, 1, 1, 1, 1, 1], [1, 2, 3, 1, 4, 5]],
-            [[0, 0, 0, 0, 0, 0], [2, 3, 4, 5, 6, 7]],
-            [[1, 1, 1, 1, 1, 1], [1, 2, 3, 1, 4, 5]]
+            [2, 3, 4, 5, 6, 7],
+            [-1, 2, -3, 1, -4, 5],
+            [-2, 3, -4, 5, -6, 7],
+            [7, -6, 7, 2, 2, -4]
         ]
         symbols = ['x', 'y', 'x', 'y']
         expected = '\n'.join([
@@ -269,20 +295,21 @@ class TestCreateAnswersTex(unittest.TestCase):
             r'& $ \speq 6x+8+30x+35$\\',
             r'& $ \speq 36x+43$\\',
             r'}{1.2cm}',
-            r'\qIIans{$-(-2y-3)-(-4y-5)$\\',
-            r'& $ \speq 2y+3+4y+5$\\',
-            r'& $ \speq 6y+8$\\',
+            r'\qIIans{$-(2y-3)+(-4y+5)$\\',
+            r'& $ \speq -2y+3-4y+5$\\',
+            r'& $ \speq -6y+8$\\',
             r'}{1.2cm}',
-            r'\qIIans{$\myfrac{1}{2}(3x+4)+\myfrac{1}{5}(6x+7)$\\',
-            r'& $ \speq \myfrac{5(3x+4)+2(6x+7)}{10}$ \fracv \\',
-            r'& $ \speq \myfrac{15x+20+12x+14}{10}$ \fracv \\',
-            r'& $ \speq \myfrac{27x+34}{10}$ \fracv \\',
+            r'\qIIans{$-\myfrac{1}{2}(3x-4)+\myfrac{1}{5}(-6x+7)$\\',
+            r'& $ \speq \myfrac{-5(3x-4)+2(-6x+7)}{10}$ \fracv \\',
+            r'& $ \speq \myfrac{-15x+20-12x+14}{10}$ \fracv \\',
+            r'& $ \speq \myfrac{-27x+34}{10}$ \fracv \\',
             r'}{3.2cm}',
-            r'\qIIans{$-(-2y-3)-(-4y-5)$\\',
-            r'& $ \speq \myfrac{-(-2y-3)-(-4y-5)}{1}$ \fracv \\',
-            r'& $ \speq \myfrac{2y+3+4y+5}{1}$ \fracv \\',
-            r'& $ \speq \myfrac{6y+8}{1}$ \fracv \\',
-            r'}{3.2cm}',
+            r'\qIIans{$\myfrac{1}{7}(-6y+7)+\myfrac{1}{2}(2y-4)$\\',
+            r'& $ \speq \myfrac{2(-6y+7)+7(2y-4)}{14}$ \fracv \\',
+            r'& $ \speq \myfrac{-12y+14+14y-28}{14}$ \fracv \\',
+            r'& $ \speq \myfrac{2y-14}{14}$ \fracv \\',
+            r'& $ \speq \myfrac{y-7}{7}$ \fracv \\',
+            r'}{4cm}',
             r'%--- end of first column -----------------------------------------',
             r'}{%',
             r'}'
